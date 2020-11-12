e9017e7b0ad025507647607478ac180ed58ebb3e
Léonard <mondet.leonard@gmail.com>
2020-11-12 15:32:15 +0100

    test

---
+++ licence_manager/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "licence manager",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Yziact",
+    'maintainer': '',
+    'website': "http://gitlab.yziact.net/odoo/commons/module",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+
+    # always loaded
+    'data': [
+        'views/product_views.xml'
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        # 'security/ir.model.access.csv',
+    ],
+}
