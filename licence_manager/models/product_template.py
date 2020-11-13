5c91325fdce4951eb07d3675771b766a72476b56
Léonard <mondet.leonard@gmail.com>
2020-11-13 14:19:24 +0100

    modif product.product

--- licence_manager/models/product_template.py
+++ licence_manager/models/product_template.py
@@ -2,8 +2,5 @@ from odoo import fields, models
 
 class ProductTemplate(models.Model):
     _name = "product.template"
-    _inherit = ['mail.thread', 'mail.activity.mixin', 'image.mixin']
-    _description = "Product Template"
-    _order = "name"
-
     licence_ok = fields.Boolean('Is a Licence', default=False)
+    
