f11f65fe626602ba7c5e91c14173af8d09f679a9
Léonard <mondet.leonard@gmail.com>
2020-11-13 14:56:27 +0100

    test modif

--- licence_manager/__manifest__.py
+++ licence_manager/__manifest__.py
@@ -3,11 +3,10 @@
     'name': "licence manager",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        MODULE DE GESTION DE LICENCE YZIACT""",
 
     'description': """
-        Long description of module's purpose
+        DESCIRIPTION EN + LONG
     """,
 
     'author': "Yziact",
@@ -27,7 +26,7 @@
 
     # always loaded
     'data': [
-        'views/product_views.xml'
+         'views/product_views.xml'
         # 'views/views.xml',
         # 'views/templates.xml',
         # 'security/ir.model.access.csv',
