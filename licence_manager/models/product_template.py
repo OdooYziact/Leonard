8ea1254d8be0244ddd2b8a52a093805032910ebc
Léonard MONDET <lmondet@yziact.fr>
2020-11-23 16:02:00 +0100

    test

--- licence_manager/models/product_template.py
+++ licence_manager/models/product_template.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
+    _inherit = 'product.template'
 
     is_licence = fields.Boolean('Is a Licence', default=False)
