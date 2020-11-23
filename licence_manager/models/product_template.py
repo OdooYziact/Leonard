713bf36ad539ff4f0fc4b518110b9ee48e609572
Léonard MONDET <lmondet@yziact.fr>
2020-11-23 15:41:10 +0100

    test

--- licence_manager/models/product_template.py
+++ licence_manager/models/product_template.py
@@ -1,9 +1,6 @@
 from odoo import fields, models
-from odoo.addons.test_impex.models import field
-
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     is_licence = fields.Boolean('Is a Licence', default=False)
-    field.Boolean(string="is a licence")
