9039ea8ad87bb4b62a684efe200f5c28a6b3650f
Léonard <mondet.leonard@gmail.com>
2020-12-04 14:48:22 +0100

    print/logg

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -1,5 +1,7 @@
 from odoo import api, models
-from pprint import pprint
+
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class SaleSubscription(models.Model):
@@ -8,7 +10,7 @@ class SaleSubscription(models.Model):
     @api.model
     def create(self, values):
         res = super(SaleSubscription, self).create(values)
-        print(values)
+        _logger.info(values)
         for line in values['recurring_invoice_line_ids']:
             if line.product_id.is_lience:
                 self.env['product.licence'].create({
@@ -16,5 +18,4 @@ class SaleSubscription(models.Model):
                     'customer_id': values['customer_id'],
                     'quantity': values['quantity'],
                 })
-        pprint(values)
         return res
