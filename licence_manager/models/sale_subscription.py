2ca1bfe74ea5d1491f4fb88edb065b0b92fe34a9
Léonard <mondet.leonard@gmail.com>
2020-12-23 19:13:03 +0100

    test product

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -10,10 +10,10 @@ class SaleSubscription(models.Model):
     @api.onchange('stage_id')
     def onchange_check_is_licence(self):
         if self.stage_id.id == 2:
-            for product, customer, quantity in self.recurring_invoice_line_ids:
+            for line_product, line_customer, line_quantity in self.recurring_invoice_line_ids:
                 if product.is_licence:
                     self.env['product.licence'].create({
-                        'product_id': product,
-                        'customer_id': customer,
-                        'quantity': quantity,
+                        'product_id': line_product,
+                        'customer_id': line_customer,
+                        'quantity': line_quantity,
                     })
