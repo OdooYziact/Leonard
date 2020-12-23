ac89214b276b1f87a943fba5eea5db1fd5d9e08a
Léonard <mondet.leonard@gmail.com>
2020-12-23 17:02:16 +0100

    test product

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -13,7 +13,7 @@ class SaleSubscription(models.Model):
             for sale_subscription_line in self.recurring_invoice_line_ids:
                 if sale_subscription_line.product_id.is_licence:
                     self.env['product.licence'].create({
-                        'product_id': int(product.product.id),
-                        'customer_id': int(),
+                        'product_id': int(),
+                        'customer_id': int(product.product.id),
                         'quantity': float(),
                     })
