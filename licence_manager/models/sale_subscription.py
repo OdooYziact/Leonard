72d7ffff98cc427ec7ea88a48a0bfca088f69184
Léonard <mondet.leonard@gmail.com>
2020-12-23 14:23:15 +0100

    test product

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -7,21 +7,11 @@ _logger = logging.getLogger(__name__)
 class SaleSubscription(models.Model):
     _inherit = "sale.subscription"
 
-    @api.model
-    def create(self, values):
-        res = super(SaleSubscription, self).create(values)
-        _logger.info(values)
-        for line in values['recurring_invoice_line_ids']:
-            if line.product_id.is_licence:
-                self.env['product.licence'].create({
-                })
-        return res
-
-################check_recovery_subscription_lines################
-
     @api.onchange('stage_id')
     def onchange_check_is_licence(self):
         if self.stage_id.id == 2:
+            res = super(SaleSubscription, self).create()
+            _logger.info()
             for line in self.recurring_invoice_line_ids:
                 if line.product_id.is_licence:
                     self.env['product.licence'].create({
@@ -29,5 +19,4 @@ class SaleSubscription(models.Model):
                         'customer_id': int(customer.id),
                         'quantity': float(quantity),
                     })
-
-### Tu dois créer une ligne de product.licence en fonction de l'article de la ligne, du client de la ligne et de la quantité de la ligne.###
+            return res
