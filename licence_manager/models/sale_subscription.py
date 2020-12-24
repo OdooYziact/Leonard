c8e06f7de326e4a523ffef777f672b53b2aac4e6
Léonard <mondet.leonard@gmail.com>
2020-12-24 14:30:45 +0100

    ajout (final, sans les erreurs de commentaire xml) de la crea avec lien tableau + commentaires explication/a faire

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -14,7 +14,7 @@ class SaleSubscription(models.Model):
                 if sale_subscription.product_id.is_licence:
                     self.env['product.licence'].create({
                         'product_id': sale_subscription.product_id.id,
-                        'customer_id': sale_subscription.customer_id.id,
+                        'customer_ids': sale_subscription.customer_ids.ids,
                         'quantity': sale_subscription.quantity.quantity,
                     })
 
