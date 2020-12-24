2d1aa2539b30ed39d5fb9ba65f83977082aefb44
Léonard <mondet.leonard@gmail.com>
2020-12-24 14:35:03 +0100

    ajout (final, sans les erreurs de commentaire xml) de la crea avec lien tableau + commentaires explication/a faire

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -14,7 +14,7 @@ class SaleSubscription(models.Model):
                 if sale_subscription.product_id.is_licence:
                     self.env['product.licence'].create({
                         'product_id': sale_subscription.product_id.id,
-                        'quantity': sale_subscription.quantity.float,
+                        'quantity': sale_subscription.quantity,
                     })
 
 #Creation d'abonnement comportant des licences + transfert d'informations dans mon tableau des le changement d'etat de
