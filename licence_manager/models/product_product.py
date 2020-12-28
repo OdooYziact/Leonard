ea23c863d20d5810b7bc280c25123adac8f55f0d
Léonard MONDET <lmondet@yziact.fr>
2020-12-28 14:40:18 +0100

    editor_id

--- licence_manager/models/product_product.py
+++ licence_manager/models/product_product.py
@@ -5,4 +5,4 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     is_licence = fields.Boolean(related='product_tmpl_id.is_licence')
-    editor_id = fields.Id(related='product_tmpl_id.editor_id')
+    editor_id = fields.Id()
