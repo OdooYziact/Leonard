98cd789fb35582671d191cc2e4ee8cc873f767cb
Léonard MONDET <lmondet@yziact.fr>
2020-12-30 14:21:11 +0100

    editor_id

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -8,14 +8,14 @@ class SaleSubscription(models.Model):
     _inherit = "sale.subscription"
 
     @api.onchange('stage_id')
-    def onchange_check_is_licence(self, res=None):
+    def onchange_check_is_licence(self):
         if self.stage_id.id == 2:
             for sale_subscription in self.recurring_invoice_line_ids:
                 if sale_subscription.product_id.is_licence:
                     self.env['product.licence'].create({
                         'product_id': sale_subscription.product_id.id,
                         'partner_id': self.partner_id.id,
-                        'editor_id': res.partner.editor_id.id,
+                        'editor_id': sale_subscription.product.editor_id.id,
                         'quantity': sale_subscription.quantity,
                     })
 
