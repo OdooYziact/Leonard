960823e3e60931f5eede6bd33be1105cdb18900d
Léonard MONDET <lmondet@yziact.fr>
2021-01-20 17:34:47 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -15,7 +15,7 @@ class SaleSubscription(models.Model):
                     self.env['product.licence'].create({
                         'product_id': sale_subscription.product_id.id,
                         'partner_id': self.partner_id.id,
-                        'editor_id': self.product_product.editor_id.id,
+                        'editor_id': sale_subscription.product_id.product_tmpl.editor_id.id,
                         'quantity': sale_subscription.quantity,
                     })
 
