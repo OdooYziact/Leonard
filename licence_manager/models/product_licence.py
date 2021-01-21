a5c8e697f2cb3a0c21cfbcd9455e5011d27a787a
Léonard MONDET <lmondet@yziact.fr>
2021-01-21 14:02:49 +0100

    test

--- licence_manager/models/product_licence.py
+++ licence_manager/models/product_licence.py
@@ -8,7 +8,7 @@ class ProductLicence(models.Model):
     product_id = fields.Many2one(string="Licence", comodel_name='product.product', readonly=True)
     partner_id = fields.Many2one(string="Partner", comodel_name='res.partner', readonly=True)
     editor_id = fields.Many2one(string="Editor", comodel_name='res.partner', readonly=True)
-    provider_id = fields.Many2many(string="Provider", comodel_name='res.partner', relation='partner_licence_rel',
+    provider_ids = fields.Many2many(string="Provider", comodel_name='res.partner', relation='partner_licence_rel',
                                     readonly=True)
     quantity = fields.Integer(string="Quantity", readonly=False)
 
