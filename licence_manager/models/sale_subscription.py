5da8848fec8ff3d9c50675ca1a30eff20ee1c3a5
Léonard MONDET <lmondet@yziact.fr>
2021-01-21 14:58:38 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -13,6 +13,7 @@ class SaleSubscription(models.Model):
             for sale_subscription in self.recurring_invoice_line_ids:
                 if sale_subscription.product_id.is_licence:
                     self.env['product.licence'].create({
+                        'subscription_id': sale_subscription.subscription.id,
                         'product_id': sale_subscription.product_id.id,
                         'partner_id': self.partner_id.id,
                         'editor_id': sale_subscription.product_id.product_tmpl_id.editor_id.id,
@@ -25,3 +26,9 @@ class SaleSubscription(models.Model):
 # Afficher plusieurs fournisseurs car si changement pendant, ca evite d'avoir un historique, in affiche jsute celui du changement
 # donc on recupere pas dans la fiche produit directement mais dans la fiche abo comme ca si on change de fournisseur en meme temps ca
 # conservze le fournsseur lors de l'achat de l'abo
+#STATUS : actif, inactif, actif a renouveller manuellement, actif va etre renouvellé, ?
+#
+#
+#Dupliquer le code onchange stage id 2 et le modif pour stage id 1,3,4, etc afin de supp (archiver), mettre en prépa, etc...
+#Utiliser l'id de la subscription line dans le tableau mais sans l'afficher
+#
