04a0f6c8aee59939ede4293b5f3b14b30f5ff937
Léonard MONDET <lmondet@yziact.fr>
2021-01-21 15:15:11 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -10,15 +10,15 @@ class SaleSubscription(models.Model):
     @api.onchange('stage_id')
     def onchange_check_is_licence(self):
         if self.stage_id.id == 2:
-            for sale_subscription in self.recurring_invoice_line_ids:
-                if sale_subscription.product_id.is_licence:
+            for line_subscription in self.recurring_invoice_line_ids:
+                if line_subscription.product_id.is_licence:
                     self.env['product.licence'].create({
-                        'subscription_id': self.recurring_invoice_line_ids.id,
-                        'product_id': sale_subscription.product_id.id,
+                        'subscription_line_id': self.line_subscription.id,
+                        'product_id': line_subscription.product_id.id,
                         'partner_id': self.partner_id.id,
-                        'editor_id': sale_subscription.product_id.product_tmpl_id.editor_id.id,
-                        'quantity': sale_subscription.quantity,
-                        'provider_ids': [(6, False, sale_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
+                        'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
+                        'quantity': line_subscription.quantity,
+                        'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
                     })
 
 
