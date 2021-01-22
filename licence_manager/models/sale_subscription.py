b973554012fd567b44e3dc62fb10dba6224ca130
Léonard MONDET <lmondet@yziact.fr>
2021-01-22 11:30:54 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -12,17 +12,15 @@ class SaleSubscription(models.Model):
         if self.stage_id.id == 2:
             for line_subscription in self.recurring_invoice_line_ids:
                 if line_subscription.product_id.is_licence:
-                    self.env['product.licence'].create({
+                    licence_id = self.env['product.licence'].create({
                         # 'subscription_line_id': line_subscription.id,
-                        'subscription_line_id': line_subscription.id,
                         'product_id': line_subscription.product_id.id,
                         'partner_id': self.partner_id.id,
                         'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
                         'quantity': line_subscription.quantity,
                         'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
                     })
-
-
+                    line_subscription.licence_id = licence_id.id
 
 # Afficher plusieurs fournisseurs car si changement pendant, ca evite d'avoir un historique, in affiche jsute celui du changement
 # donc on recupere pas dans la fiche produit directement mais dans la fiche abo comme ca si on change de fournisseur en meme temps ca
@@ -32,4 +30,9 @@ class SaleSubscription(models.Model):
 #
 #Dupliquer le code onchange stage id 2 et le modif pour stage id 1,3,4, etc afin de supp (archiver), mettre en prépa, etc...
 #Utiliser l'id de la subscription line dans le tableau mais sans l'afficher
-#
+
+
+class SaleSubscriptionLine(models.Model):
+    _inherit = 'sale.subscription.line'
+
+    licence_id = fields.Many2one(comodel_name='product.licence')
