0d1703699b246cf9728b2fe5fa98912fe7409535
Léonard MONDET <lmondet@yziact.fr>
2021-01-26 17:17:44 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -24,13 +24,8 @@ class SaleSubscription(models.Model):
         if self.stage_id.id == 3:
             for line_subscription in self.recurring_invoice_line_ids:
                 if line_subscription.product_id.is_licence:
-                    licence_id = self.licence.id.unlink({
+                    licence_id = self.env['product.licence'].unlink({
                         'subscription_line_id': line_subscription._origin.id,
-                        'product_id': line_subscription.product_id.id,
-                        'partner_id': self.partner_id.id,
-                        'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
-                        'quantity': line_subscription.quantity,
-                        'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
                     })
                     line_subscription.licence_id = licence_id.id
 
