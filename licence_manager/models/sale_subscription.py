8c0b39898699999dceb65afb366cac1c380ab121
Léonard MONDET <lmondet@yziact.fr>
2021-01-26 16:47:28 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -23,18 +23,19 @@ class SaleSubscription(models.Model):
                     line_subscription.licence_id = licence_id.id
 
     def onchange_check_is_licence_dead(self):
-        if self.stage_id.id == 3:
-            for line_subscription in self.recurring_invoice_line_ids:
-                if line_subscription.product_id.is_licence:
-                    licence_id = self.env['product.licence'].unlink({
-                        'subscription_line_id': line_subscription._origin.id,
-                        'product_id': line_subscription.product_id.id,
-                        'partner_id': self.partner_id.id,
-                        'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
-                        'quantity': line_subscription.quantity,
-                        'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
-                    })
-                    line_subscription.licence_id = licence_id.id
+        if self.stage_id.id == 2:
+            if self.stage_id.id == 3:
+                for line_subscription in self.recurring_invoice_line_ids:
+                    if line_subscription.product_id.is_licence:
+                        licence_id = self.env['product.licence'].unlink({
+                            'subscription_line_id': line_subscription._origin.id,
+                            'product_id': line_subscription.product_id.id,
+                            'partner_id': self.partner_id.id,
+                            'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
+                            'quantity': line_subscription.quantity,
+                            'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
+                        })
+                        line_subscription.licence_id = licence_id.id
 
 
 class SaleSubscriptionLine(models.Model):
