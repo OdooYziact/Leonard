06bb4422d38d6638b533ece394a9f86bebf7eea5
Léonard <mondet.leonard@gmail.com>
2021-01-29 14:54:05 +0100

    test

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -27,9 +27,20 @@ class SaleSubscriptionLine(models.Model):
 
     licence_id = fields.Many2one(comodel_name='product.licence')
 
-   # def onchange_delete(self):
-   #     if self.stage_id.id == 3:
-   #         for line_subscription in self.recurring_invoice_line_ids:
-   #             if line_subscription.product_id.is_licence:
-   #                 licence_id = self.env['product.licence'].unlink()
-   #                 line_subscription.licence_id = licence_id
+class SaleSubscription_(models.Model):
+    _inherit = 'sale.subscription'
+
+    @api.onchange('stage_id')
+    def onchange_check_is_licence_dead(self):
+        if self.stage_id.id == 3:
+            for line_subscription in self.recurring_invoice_line_ids:
+                if line_subscription.product_id.is_licence:
+                    licence_id = self.env['product.licence'].unlink({
+                        'subscription_line_id': line_subscription._origin.id,
+                        'product_id': line_subscription.product_id.id,
+                        'partner_id': self.partner_id.id,
+                        'editor_id': line_subscription.product_id.product_tmpl_id.editor_id.id,
+                        'quantity': line_subscription.quantity,
+                        'provider_ids': [(6, False, line_subscription.product_id.product_tmpl_id.seller_ids.mapped('name').ids)],
+                    })
+                    line_subscription.licence_id = licence_id.id
