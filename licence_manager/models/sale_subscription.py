e64035116ed79c055b93d63c52c1d39eded6408e
Léonard MONDET <lmondet@yziact.fr>
2021-02-17 11:30:19 +0100

    test stage

--- licence_manager/models/sale_subscription.py
+++ licence_manager/models/sale_subscription.py
@@ -38,3 +38,7 @@ class SaleSubscriptionLine(models.Model):
 
 #track_visibility odoo : history
 
+class SaleSubsriptionStage(models.Model):
+    _inherit = 'sale.subscription.stage'
+
+    status = fields.Selection([('Avant Vente', 'En cour', 'Annulé')])
